d5f83b6034d8ba0f5964945676e2b6adc6fc9a2a
Your Name <you@example.com>
2023-11-16 20:25:24 +0700

    perintah tugas 8 baris 1

--- belajar PHP/index.php
+++ belajar PHP/index.php
@@ -1,20 +1,6 @@
 <?php
 
-$x = 10;
-$x += 2;    #operator assignment +=
-$y = 10;
-$y -= 2;    #operator assignment -=
-$z = 10;
-$z /= 2;    #operator assignment /=
-$a = 10;
-$a %= 2;    #operator assignment %=
-$b = 10;
-$b .= 2;    #operator assignment .=
-
-echo $x . "</br>";
-echo $y . "</br>";
-echo $z . "</br>";
-echo $a . "</br>";
-echo $b . "</br>";
+var_dump (1<5);
+echo "</br>";   #output baris ke-1
 
 ?>
