1356d17f1f0f46227f164dba273cd6dbdab9e909
Your Name <you@example.com>
2023-11-16 19:09:26 +0700

    perintah tugas 6

--- belajar PHP/index.php
+++ belajar PHP/index.php
@@ -1,11 +1,8 @@
 <?php
-//Operator Aritmatika
-$x = 10;
-$y = 20;
-    echo $x + $y ;  #operator penjumlahan
-    echo $x - $y ;  #operator pengurangan
-    echo $x * $y ;  #operator perkalian
-    echo $x / $y ;  #operator pembagian
-    echo $x % $y ;  #operator modulus
+
+$nama_depan = "Komaruddin";
+$nama_belakang = "Zuhdi";
+
+echo $nama_depan . "" . $nama_belakang;
 
 ?>
