293cb11c26b3ed93d26ff5c53a3dad7d017476d0
Your Name <you@example.com>
2023-11-16 20:18:02 +0700

    perintah tugas 7 semuanya muncul

--- belajar PHP/index.php
+++ belajar PHP/index.php
@@ -1,7 +1,20 @@
 <?php
 
 $x = 10;
-$x .= 2;    #operator assignment .=
+$x += 2;    #operator assignment +=
+$y = 10;
+$y -= 2;    #operator assignment -=
+$z = 10;
+$z /= 2;    #operator assignment /=
+$a = 10;
+$a %= 2;    #operator assignment %=
+$b = 10;
+$b .= 2;    #operator assignment .=
 
 echo $x . "</br>";
+echo $y . "</br>";
+echo $z . "</br>";
+echo $a . "</br>";
+echo $b . "</br>";
+
 ?>
